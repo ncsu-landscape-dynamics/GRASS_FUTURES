917794765bc67788207f8f769998d130950fa29f
Anna Petrasova <kratochanna@gmail.com>
2015-07-03 12:58:33 -0400

    fix licence, add not in manual

--- r.futures/r.futures.devpressure/r.futures.devpressure.py
+++ r.futures/r.futures.devpressure/r.futures.devpressure.py
@@ -12,7 +12,7 @@
 # COPYRIGHT:    (C) 2015 by the GRASS Development Team
 #
 #		This program is free software under the GNU General Public
-#		License (version 2). Read the file COPYING that comes with GRASS
+#		License (>=v2). Read the file COPYING that comes with GRASS
 #		for details.
 #
 ##############################################################################
