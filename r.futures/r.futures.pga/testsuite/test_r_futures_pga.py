ab8a0f5ba3501a2f2c09fb28d2eb0a1ea209cf59
Anna Petrasova <kratochanna@gmail.com>
2019-05-13 10:41:33 -0400

    update testsuite, add example based on ncspm

--- r.futures/r.futures.pga/testsuite/test_r_futures_pga.py
+++ r.futures/r.futures.pga/testsuite/test_r_futures_pga.py
@@ -5,34 +5,35 @@ from grass.gunittest.main import test
 class TestPGA(TestCase):
 
     output = 'pga_output'
+    result = 'result'
 
     @classmethod
     def setUpClass(cls):
         cls.use_temp_region()
-        cls.runModule('g.region', n=1595760, s=1494780, e=1608600, w=1509630, res=30)
+        cls.runModule('g.region', raster="lsat7_2002_30@PERMANENT")
+        cls.runModule('r.unpack', input='data/result.pack', output=cls.result)
 
     @classmethod
     def tearDownClass(cls):
         cls.del_temp_region()
 
     def tearDown(cls):
-        """Remove viewshed map after each test method"""
         # TODO: eventually, removing maps should be handled through testing framework fucntions
         cls.runModule('g.remove', flags='f', type='raster',
-                      name=cls.output)
+                      name=[cls.output, cls.result])
 
     def test_pga_run(self):
         """Test if results is in expected limits"""
-        self.assertModule('r.futures.pga', developed='urban_2010', development_pressure='devpressure_0_5',
-                          compactness_mean=0.4, compactness_range=0.1, discount_factor=0.6,
+        self.assertModule('r.futures.pga', developed='urban_2002', development_pressure='devpressure',
+                          compactness_mean=0.4, compactness_range=0.05, discount_factor=0.1,
                           patch_sizes='data/patches.txt',
-                          predictors=['euc_interchanges_100', 'euc_protect', 'forest_1km_100', 'rdens_1km_100', 'water_dist'],
-                          n_dev_neighbourhood=10, devpot_params='data/potential_FIPS.csv',
-                          incentive_table='data/incentive_table.txt', random_seed=1,
+                          predictors=['slope', 'lakes_dist_km', 'streets_dist_km'],
+                          n_dev_neighbourhood=15, devpot_params='data/potential.csv',
+                          random_seed=1,
                           num_neighbors=4, seed_search=2, development_pressure_approach='gravity',
-                          gamma=0.5, scaling_factor=1, subregions='selected_counties_id',
-                          demand='data/demand_noheader.csv', output=self.output)
-        self.assertRastersNoDifference(actual=self.output, reference='urban_2030', precision=1e-6)
+                          gamma=1.5, scaling_factor=1, subregions='zipcodes',
+                          demand='data/demand.csv', output=self.output)
+        self.assertRastersNoDifference(actual=self.output, reference=self.result, precision=1e-6)
 
 
 if __name__ == '__main__':
