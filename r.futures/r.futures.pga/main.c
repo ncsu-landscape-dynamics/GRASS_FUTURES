b82fe730755d207fb4ef05f873c7d48b91ff48e2
Anna Petrasova <kratochanna@gmail.com>
2019-05-21 23:11:38 -0400

    continue

--- r.futures/r.futures.pga/main.c
+++ r.futures/r.futures.pga/main.c
@@ -35,12 +35,12 @@
 
 /**
     \file main.c
-
+    
     The main file containing both the model code and the data handing part.
-
+    
     The language of the code is subject to change. The goal is to use either
     C or C++, not both mixed as it is now. Update: only C is used now.
-
+    
     Major refactoring of the code is expected.
 */
 
@@ -90,8 +90,8 @@ void readSubregions(const char *subregions, SEGMENT * segment,
     int segment_cols = 64;
     int segments_in_memory = 4;
     if (Segment_open(segment, G_tempfile(), Rast_window_rows(),
-                         Rast_window_cols(), segment_rows, segment_cols,
-                         Rast_cell_size(CELL_TYPE), segments_in_memory) != 1)
+                     Rast_window_cols(), segment_rows, segment_cols,
+                     Rast_cell_size(CELL_TYPE), segments_in_memory) != 1)
         G_fatal_error(_("Cannot create temporary file with segments of a raster map"));
     for (row = 0; row < Rast_window_rows(); row++) {
         Rast_get_row(fd, buffer, row, CELL_TYPE);
@@ -101,7 +101,7 @@ void readSubregions(const char *subregions, SEGMENT * segment,
              ptr = G_incr_void_ptr(ptr, Rast_cell_size(CELL_TYPE))) {
             if (Rast_is_null_value(ptr, CELL_TYPE))
                 ;
-                //Rast_set_c_null_value(ptr, 1);
+            //Rast_set_c_null_value(ptr, 1);
             else {
                 val = *(CELL *) ptr;
                 if (KeyValueIntInt_find(region_map, val, &index))
@@ -111,11 +111,12 @@ void readSubregions(const char *subregions, SEGMENT * segment,
                     index = count_regions;
                     count_regions++;
                 }
+                *(CELL *) ptr = index;
             }
-            *(CELL *) ptr = index;
         }
         Segment_put_row(segment, buffer, row);
     }
+    Segment_flush(segment);
     G_free(buffer);
     Rast_close(fd);
 }
@@ -132,9 +133,9 @@ void readDemandFile(struct demand *demandInfo, struct KeyValueIntInt *region_map
     for (char c = getc(fp); c != EOF; c = getc(fp)) 
         if (c == '\n') // Increment count if this character is newline 
             countlines++; 
-      
+
     rewind(fp);
-    
+
     size_t buflen = 4000;
     char buf[buflen];
     if (G_getl2(buf, buflen, fp) == 0)
@@ -157,8 +158,8 @@ void readDemandFile(struct demand *demandInfo, struct KeyValueIntInt *region_map
     // skip first column which does not contain id of the region
     int i;
     for (i = 1; i < ntokens; i++) {
-            G_chop(tokens[i]);
-            G_ilist_add(ids, atoi(tokens[i]));
+        G_chop(tokens[i]);
+        G_ilist_add(ids, atoi(tokens[i]));
     }
 
     int years = 0;
@@ -193,8 +194,8 @@ void readDemandFile(struct demand *demandInfo, struct KeyValueIntInt *region_map
     demandInfo->max_subregions = region_map->nitems;
     demandInfo->max_steps = years;
     G_verbose_message("Number of steps in demand file: %d", years);
-//    if (!sParams.nSteps)
-//        sParams.nSteps = years;
+    //    if (!sParams.nSteps)
+    //        sParams.nSteps = years;
     G_free_ilist(ids);
     G_free_tokens(tokens);
 }
@@ -207,11 +208,11 @@ void readDemandFile(struct demand *demandInfo, struct KeyValueIntInt *region_map
 
 int main(int argc, char **argv)
 {
-
+    
     struct
     {
         struct Option
-            *developed, *subregions, *predictors, *devpressure, *demandFile,
+                *developed, *subregions, *predictors, *devpressure, *demandFile,
                 *output, *seed;
 
     } opt;
@@ -224,9 +225,8 @@ int main(int argc, char **argv)
     struct input *predictors = NULL;
     int num_predictors;
     struct KeyValueIntInt *region_map;
-//    int devDemands[MAXNUM_COUNTY][MAX_YEARS];
+    //    int devDemands[MAXNUM_COUNTY][MAX_YEARS];
     struct demand demandInfo;
-
 
     G_gisinit(argv[0]);
 
@@ -238,16 +238,16 @@ int main(int argc, char **argv)
     G_add_keyword(_("landscape"));
     G_add_keyword(_("modeling"));
     module->label =
-        _("Simulates landuse change using FUTure Urban-Regional Environment Simulation (FUTURES).");
+            _("Simulates landuse change using FUTure Urban-Regional Environment Simulation (FUTURES).");
     module->description =
-        _("Module uses Patch-Growing Algorithm (PGA) to"
-          " simulate urban-rural landscape structure development.");
-
+            _("Module uses Patch-Growing Algorithm (PGA) to"
+              " simulate urban-rural landscape structure development.");
+    
     opt.developed = G_define_standard_option(G_OPT_R_INPUT);
     opt.developed->key = "developed";
     opt.developed->required = YES;
     opt.developed->description =
-        _("Raster map of developed areas (=1), undeveloped (=0) and excluded (no data)");
+            _("Raster map of developed areas (=1), undeveloped (=0) and excluded (no data)");
     opt.developed->guisection = _("Basic input");
 
     opt.subregions = G_define_standard_option(G_OPT_R_INPUT);
@@ -255,7 +255,7 @@ int main(int argc, char **argv)
     opt.subregions->required = YES;
     opt.subregions->description = _("Raster map of subregions");
     opt.subregions->guisection = _("Basic input");
-    
+
     opt.predictors = G_define_standard_option(G_OPT_R_INPUTS);
     opt.predictors->key = "predictors";
     opt.predictors->required = YES;
@@ -268,21 +268,21 @@ int main(int argc, char **argv)
     opt.devpressure->key = "development_pressure";
     opt.devpressure->required = YES;
     opt.devpressure->description =
-        _("Raster map of development pressure");
+            _("Raster map of development pressure");
     opt.devpressure->guisection = _("Development pressure");
 
     opt.output = G_define_standard_option(G_OPT_R_OUTPUT);
     opt.output->key = "output";
     opt.output->required = YES;
     opt.output->description =
-        _("State of the development at the end of simulation");
+            _("State of the development at the end of simulation");
     opt.output->guisection = _("Output");
 
     opt.demandFile = G_define_standard_option(G_OPT_F_INPUT);
     opt.demandFile->key = "demand";
     opt.demandFile->required = YES;
     opt.demandFile->description =
-        _("Control file with number of cells to convert");
+            _("Control file with number of cells to convert");
     opt.demandFile->guisection = _("Demand");
 
     opt.seed = G_define_option();
@@ -291,22 +291,21 @@ int main(int argc, char **argv)
     opt.seed->required = NO;
     opt.seed->label = _("Seed for random number generator");
     opt.seed->description =
-        _("The same seed can be used to obtain same results"
-          " or random seed can be generated by other means.");
+            _("The same seed can be used to obtain same results"
+              " or random seed can be generated by other means.");
     opt.seed->guisection = _("Random numbers");
 
     flg.generateSeed = G_define_flag();
     flg.generateSeed->key = 's';
     flg.generateSeed->label =
-        _("Generate random seed (result is non-deterministic)");
+            _("Generate random seed (result is non-deterministic)");
     flg.generateSeed->description =
-        _("Automatically generates random seed for random number"
-          " generator (use when you don't want to provide the seed option)");
+            _("Automatically generates random seed for random number"
+              " generator (use when you don't want to provide the seed option)");
     flg.generateSeed->guisection = _("Random numbers");
 
     // TODO: add mutually exclusive?
     // TODO: add flags or options to control values in series and final rasters
-
 
     // provided XOR generated
     G_option_exclusive(opt.seed, flg.generateSeed, NULL);
@@ -338,94 +337,89 @@ int main(int argc, char **argv)
     int segment_rows = 64;
     int segment_cols = 64;
     int segments_in_memory = 4;
-    
+
     //   read developed
     G_verbose_message("Reading input rasters...");
     int rowio = Rast_open_old(opt.developed->answer, "");
     if (Segment_open(&developed_segment, G_tempfile(), Rast_window_rows(),
-                         Rast_window_cols(), segment_rows, segment_cols,
-                         Rast_cell_size(CELL_TYPE), segments_in_memory) != 1)
+                     Rast_window_cols(), segment_rows, segment_cols,
+                     Rast_cell_size(CELL_TYPE), segments_in_memory) != 1)
         G_fatal_error(_("Cannot create temporary file with segments of a raster map"));
     void *raster_row = Rast_allocate_buf(CELL_TYPE);
     for (int row = 0; row < Rast_window_rows(); row++) {
         Rast_get_row(rowio, raster_row, row, CELL_TYPE);
         for (int col = 0; col < Rast_window_cols(); col++) {
-            CELL c = ((CELL *) raster_row)[col];
-            ((CELL *) raster_row)[col] = c - 1;
+            if (Rast_is_null_value(&((CELL *) raster_row)[col], CELL_TYPE))
+                ;
+            else {
+                CELL c = ((CELL *) raster_row)[col];
+                ((CELL *) raster_row)[col] = c - 1;
+            }
         }
         Segment_put_row(&developed_segment, raster_row, row);
     }
     Segment_flush(&developed_segment);
     Rast_close(rowio);
     G_free(raster_row);
-    
-//    read Subregions layer
+
+    //    read Subregions layer
     region_map = KeyValueIntInt_create();
     readSubregions(opt.subregions->answer, &subregions_segment, region_map);
 
-//    development pressure
-        rowio = Rast_open_old(opt.devpressure->answer, "");
-        if (Segment_open(&devpressure_segment, G_tempfile(), Rast_window_rows(),
-                             Rast_window_cols(), segment_rows, segment_cols,
-                             Rast_cell_size(FCELL_TYPE), segments_in_memory) != 1)
-            G_fatal_error(_("Cannot create temporary file with segments of a raster map"));
-        raster_row = Rast_allocate_buf(FCELL_TYPE);
-        for (int row = 0; row < Rast_window_rows(); row++) {
-            Rast_get_row(rowio, raster_row, row, FCELL_TYPE);
-            Segment_put_row(&devpressure_segment, raster_row, row);
-        }
-        Segment_flush(&devpressure_segment);
-        Rast_close(rowio);
-        G_free(raster_row);
+    //    development pressure
+    rowio = Rast_open_old(opt.devpressure->answer, "");
+    if (Segment_open(&devpressure_segment, G_tempfile(), Rast_window_rows(),
+                     Rast_window_cols(), segment_rows, segment_cols,
+                     Rast_cell_size(FCELL_TYPE), segments_in_memory) != 1)
+        G_fatal_error(_("Cannot create temporary file with segments of a raster map"));
+    raster_row = Rast_allocate_buf(FCELL_TYPE);
+    for (int row = 0; row < Rast_window_rows(); row++) {
+        Rast_get_row(rowio, raster_row, row, FCELL_TYPE);
+        Segment_put_row(&devpressure_segment, raster_row, row);
+    }
+    Segment_flush(&devpressure_segment);
+    Rast_close(rowio);
+    G_free(raster_row);
 
-//    num_predictors = 0;
-//    for (int i = 0; opt.predictors->answers[i]; i++)
-//        num_predictors++;
+    //    num_predictors = 0;
+    //    for (int i = 0; opt.predictors->answers[i]; i++)
+    //        num_predictors++;
+    
+    //    predictors = G_malloc(num_predictors * sizeof(struct input));
+    
+    //    for (i = 0; i < num_inputs; i++) {
+    //        struct input *p = &predictors[i];
+    //        p->name = opt.predictors->answers[i];
+    //        p->fd = Rast_open_old(p->name, "");
+    //        if (p->fd < 0)
+    //            G_fatal_error(_("Unable to open input raster <%s>"), p->name);
+    //    }
 
-//    predictors = G_malloc(num_predictors * sizeof(struct input));
-    
-//    for (i = 0; i < num_inputs; i++) {
-//        struct input *p = &predictors[i];
-//        p->name = opt.predictors->answers[i];
-//        p->fd = Rast_open_old(p->name, "");
-//        if (p->fd < 0)
-//            G_fatal_error(_("Unable to open input raster <%s>"), p->name);
-//    }
-    
-    
-    
-    
     /* read Demand file */
     demandInfo.filename = opt.demandFile->answer;
     readDemandFile(&demandInfo, region_map);
-    
-    
-    
-    
+
     /* here do the modeling */
-    
-    
-    
+
     /* write */
     int fd = Rast_open_new(opt.output->answer, CELL_TYPE);
     void *out_buffer = Rast_allocate_buf(CELL_TYPE);
 
     for (int row = 0; row < Rast_window_rows(); row++) {
-        Segment_get_row(&developed_segment, &out_buffer, row);
+        Segment_get_row(&developed_segment, out_buffer, row);
         Rast_put_row(fd, out_buffer, CELL_TYPE);
     }
-    G_free(out_buffer);
     Rast_close(fd);
-    
+    G_free(out_buffer);
     Segment_close(&developed_segment);
-    
+
     KeyValueIntInt_free(region_map);
     if (demandInfo.table) {
         for (int i = 0; i < demandInfo.max_subregions; i++)
             G_free(demandInfo.table[i]);
         G_free(demandInfo.table);
     }
-    
+
     return EXIT_SUCCESS;
 }
 
