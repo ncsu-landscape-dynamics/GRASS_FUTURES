c70ce59eacf199fe70d227eff083619c86e3b976
Anna Petrasova <kratochanna@gmail.com>
2019-11-25 13:34:12 -0500

    Update copy_to_grass_addons.sh

--- copy_to_grass_addons.sh
+++ copy_to_grass_addons.sh
@@ -6,8 +6,8 @@
 set -e
 
 if [ $# -lt 1 ] ; then
-    echo "Provide directory of the addon in a SVN copy as a parameter"
-    echo "Usage: $0 /path/to/svn/addons/grass7/raster/r.futures"
+    echo "Provide directory of the addon in a Git clone of grass-addons repo as a parameter"
+    echo "Usage: $0 /path/to/git/addons/grass7/raster/r.futures"
     echo "Run in the directory which is a clone of the Git repository"
     exit 1
 fi
