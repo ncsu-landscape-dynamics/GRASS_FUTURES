2618d20a07b4013bc69aadb646911ce274d4c49d
Anna Petrasova <kratochanna@gmail.com>
2019-11-26 09:22:30 -0500

    update copy script

--- copy_to_grass_addons.sh
+++ copy_to_grass_addons.sh
@@ -8,11 +8,13 @@ set -e
 if [ $# -lt 1 ] ; then
     echo "Provide directory of the addon in a Git clone of grass-addons repo as a parameter"
     echo "Usage: $0 /path/to/git/addons/grass7/raster/r.futures"
-    echo "Run in the directory which is a clone of the Git repository"
+    echo "Run in the directory which is a clone of the FUTURES Git repository"
     exit 1
 fi
 
-cp -r r.futures/* $1
-rm $1/r.futures.pga/Doxyfile
-rm $1/r.futures.pga/incentive.svg
-rm $1/r.futures.pga/futures_indent.sh
+git archive HEAD r.futures/ | tar -x -C /tmp
+rm /tmp/r.futures/r.futures.pga/Doxyfile
+rm /tmp/r.futures/r.futures.pga/incentive.svg
+rm /tmp/r.futures/r.futures.pga/futures_indent.sh
+cp -r /tmp/r.futures/* $1
+rm -rf /tmp/r.futures/
